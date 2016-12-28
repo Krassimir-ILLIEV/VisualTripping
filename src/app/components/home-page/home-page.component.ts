7f3f7b5a41a2984073a5baec902ec107dede1516
pavelhristov <pavel.hristov_@abv.bg>
2016-12-29 01:44:25 +0200

    home page slider

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: `
-        <h1>Hello</h1>
-    `
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-
+    public images = IMAGES;
 }
+
+const IMAGES: any[] = [
+    { 'title': 'first', 'url': 'http://www.intrawallpaper.com/static/images/wallpaper-photos-17.jpg' },
+    { 'title': '2', 'url': 'http://www.intrawallpaper.com/static/images/wallpaper-photos-17.jpg' },
+    { 'title': '3', 'url': 'http://www.intrawallpaper.com/static/images/wallpaper-photos-17.jpg' },
+    { 'title': '4', 'url': 'http://www.intrawallpaper.com/static/images/wallpaper-photos-17.jpg' },
+    { 'title': '5', 'url': 'http://www.intrawallpaper.com/static/images/wallpaper-photos-17.jpg' },
+];
