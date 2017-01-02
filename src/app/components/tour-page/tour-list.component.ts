d11ef7e90737e8d970a328989946da5a57e01e2e
Krassimir-ILLIEV <krassimir.illiev@mail.bg>
2017-01-02 13:35:57 +0200

    more responsive form, separate html files

--- src/app/components/tour-page/tour-list.component.ts
+++ src/app/components/tour-page/tour-list.component.ts
@@ -4,76 +4,7 @@ import { Tour } from '../../../models/tour.model';
 
 @Component({
   selector: 'tour-list-container',
-  template: `
-  <div class="well">
-    <div class="row">
-      <div class="col-xs-2 form-group">
-          <label for="sort">Sort by:</label>
-          </div>
-          <div class="col-xs-3 form-group">
-          <select class="form-control" name="sort"
-          [(ngModel)]="fieldToSort">
-          <option *ngFor="let f of fieldsToSort" [value]="f.val">{{f.display}}</option>
-          </select>
-      </div>
-      <div class="col-xs-2 form-group">
-          <label for="order">Set order:</label>
-          </div>
-          <div class="col-xs-2 form-group">
-          <select class="form-control" name="order"
-          [(ngModel)]="orderToSort">
-          <option *ngFor="let f of ordersToSort" [value]="f">{{f}}</option>
-          </select>
-      </div>
-      
-      <div class="col-xs-offset-2 col-xs-1">
-        <div class="pull-right">
-          <span class="glyphicon glyphicon-plus-sign" [routerLink]="['/tours', '0']"></span>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <table class="col-xs-12 table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th class="hidden-xs">Description</th>
-            <th>Creator</th>
-            <th>Places...</th>
-            <th class="hidden-xs">Start Date</th>
-            <th class="hidden-xs">Duration in Days</th>
-            <th class="hidden-xs">Total Price</th>
-            <th class="hidden-xs">Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr *ngFor="let tour of tours | tourFilterByPlace:listFilter.place 
-          | tourFilterByDescription:listFilter.description
-          | tourFilterByAfterDate:listFilter.after
-          | tourFilterByBeforeDate:listFilter.before
-          | tourSort:fieldToSort:orderToSort">
-            <td class="hidden-xs">{{tour.description}}...</td>
-            <td>{{tour.creator}}</td>
-            <td>
-              {{tour.tourPoints[0].city}}
-              <span class="hidden-sm hidden-md hidden-lg pull-right glyphicon glyphicon-pencil"
-                [routerLink]="['/tours', tour._id]">
-              </span>
-            </td>
-            <td class="hidden-xs">{{tour.tourPoints[0].startDate | date}}</td>
-            <td class="hidden-xs">{{getDuration(tour)}}</td>
-            <td class="hidden-xs">{{tour.price}}</td>
-            <td class="hidden-xs">
-              {{tour.rating}}
-              <span class="pull-right glyphicon glyphicon-pencil"
-                [routerLink]="['/tours', tour._id]">
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-  `
+  templateUrl: './tour-list.component.html'
 })
 export class TourListComponent implements OnInit {
   fieldToSort = '';
--- src/app/components/tour-page/tour-page.component.ts
+++ src/app/components/tour-page/tour-page.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tour-page',
-  template: `
-  <div class="container-fluid">
-    <div class="row">
-      <search-tour-container class="col-md-4" (onSearch)="onSearch($event)"></search-tour-container>
-      
-      <tour-list-container class="col-md-8" [listFilter]='search_'></tour-list-container>
-    </div>
-  </div>
-  `
+  templateUrl: './tour-page.component.html'
 })
 export class TourPageComponent {
   search_ = {};
